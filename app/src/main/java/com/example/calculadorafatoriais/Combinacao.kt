73b922d7eb52b53dcd0ec11b393c852d255ff1a3
Roberto Martins <robertomartins@id.uff.br>
2023-04-09 20:17:29 -0300

    Faz com que editTexts que se referem ao mesmo valor sempre tenham mesmo texto

--- app/src/main/java/com/example/calculadorafatoriais/Combinacao.kt
+++ app/src/main/java/com/example/calculadorafatoriais/Combinacao.kt
@@ -1,11 +1,14 @@
 package com.example.calculadorafatoriais
 
 import android.os.Bundle
+import android.text.Editable
 import android.text.SpannableString
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.example.calculadorafatoriais.databinding.FragmentCombinacaoBinding
@@ -29,6 +32,27 @@ class Combinacao : Fragment() {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentCombinacaoBinding>(inflater, R.layout.fragment_combinacao, container, false)
         // binding.numeratorNEditText.addTextChangedListener()
+
+        setUpTextChangeListener(binding.numeratorNEditText, binding.nSubtractionEditText)
+        setUpTextChangeListener(binding.nSubtractionEditText, binding.numeratorNEditText)
+        setUpTextChangeListener(binding.kDivisorEditText, binding.kSubtractionEditText)
+        setUpTextChangeListener(binding.kSubtractionEditText, binding.kDivisorEditText)
+
         return binding.root
+    }
+
+    // listenedView é view que receberá listener, affectedView será a view alterada pela função do listener
+    private fun setUpTextChangeListener(listenedView: EditText, affectedView: EditText) {
+        listenedView.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (affectedView.text.toString() != listenedView.text.toString()) {
+                    affectedView.text = listenedView.text
+                }
+            }
+        })
     }
 }
