9a9fe56fc0c3566773a805b645d4cf75d41e59f5
Roberto Martins <robertomartins@id.uff.br>
2023-04-09 20:14:35 -0300

    Faz com que editTexts que se referem ao mesmo valor sempre tenham mesmo texto

--- app/src/main/java/com/example/calculadorafatoriais/Arranjo.kt
+++ app/src/main/java/com/example/calculadorafatoriais/Arranjo.kt
@@ -1,14 +1,20 @@
 package com.example.calculadorafatoriais
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.text.Editable
 import android.text.SpannableString
 import android.text.Spanned
+import android.text.TextWatcher
 import android.text.style.SubscriptSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.example.calculadorafatoriais.databinding.FragmentArranjoBinding
 
@@ -16,11 +22,10 @@ import com.example.calculadorafatoriais.databinding.FragmentArranjoBinding
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 class Arranjo : Fragment() {
-    // TODO: Rename and change types of parameters
+    lateinit var binding : FragmentArranjoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -28,10 +33,51 @@ class Arranjo : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentArranjoBinding>(inflater, R.layout.fragment_arranjo, container, false)
-        val formulaDefString : SpannableString =  SpannableString("Ank = ")
-        formulaDefString.setSpan(SubscriptSpan(), 1, 3, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        binding.arranjoTextView.setText(formulaDefString, TextView.BufferType.SPANNABLE)
+        binding = DataBindingUtil.inflate<FragmentArranjoBinding>(inflater, R.layout.fragment_arranjo, container, false)
+        writeFormula("n", "k")
+        setUpTextChangeListener(binding.numeradorEditView, binding.nDivisorEditView)
+        setUpTextChangeListener(binding.nDivisorEditView, binding.numeradorEditView)
+
+        binding.kDivisorEditView.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+            override fun afterTextChanged(s: Editable?) {
+                var n = "n"
+                /* variável n tem valor "n" se nenhum editText com valor de n tiver sido preenchido,
+                caso contrário, terá valor do campo */
+                if (binding.numeradorEditView.text.toString() != "" || binding.nDivisorEditView.text.toString() != "") {
+                    n = if (binding.numeradorEditView.text.toString() != "") binding.numeradorEditView.text.toString()
+                        else binding.nDivisorEditView.text.toString()
+                }
+                val k = if (binding.kDivisorEditView.text.toString() != "") binding.kDivisorEditView.text.toString() else "k"
+                writeFormula(n, k)
+            }
+        })
+
         return binding.root
+    }
+
+    private fun writeFormula(n: String, k: String) {
+        val formulaDefString : SpannableString =  SpannableString("A${n},${k} = ")
+        formulaDefString.setSpan(SubscriptSpan(), 1, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.arranjoTextView.setText(formulaDefString, TextView.BufferType.SPANNABLE)
+    }
+
+    // listenedView é view que receberá listener, affectedView será a view alterada pela função do listener
+    private fun setUpTextChangeListener(listenedView: EditText, affectedView: EditText) {
+        listenedView.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (affectedView.text.toString() != listenedView.text.toString()) {  // Necessário para evitar recursão
+                    affectedView.text = listenedView.text
+                    val k = if (binding.kDivisorEditView.text.toString() == "") "k" else binding.kDivisorEditView.text.toString()
+                    val n = if (listenedView.text.toString() == "") "n" else listenedView.text.toString()
+                    writeFormula(n, k)
+                }
+            }
+        })
     }
 }
