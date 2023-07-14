47bad632cbd61ed41fee8bab5833529a27825fbc
Roberto Martins <robertomartins@id.uff.br>
2023-07-14 13:19:46 -0300

    Implementa configuração de layout do layout por interface

--- app/src/main/java/com/example/calculadorafatoriais/explanationWidgets/ParentLinearLayout.kt
+++ app/src/main/java/com/example/calculadorafatoriais/explanationWidgets/ParentLinearLayout.kt
@@ -1,19 +1,14 @@
 package com.example.calculadorafatoriais.explanationWidgets
 
-import android.app.ActionBar
 import android.content.Context
-import android.util.TypedValue
 import android.widget.LinearLayout
+import com.example.calculadorafatoriais.SetLayoutParams
 
 /** Layout usado como Layout Pai para fórmulas de Análise Combinatória. Define seus parâmetros de
  * layout e margens em seu construtor. */
-class ParentLinearLayout(context: Context) : LinearLayout(context) {
+open class ParentLinearLayout(context: Context) : LinearLayout(context), SetLayoutParams {
     init {
-        // Configurando parametros para layout da classe
-        val params = ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT)
-        fun dpToPx(context: Context, size: Float): Int { return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size, context.resources.displayMetrics).toInt() }
-        params.setMargins(dpToPx(context, 20F), dpToPx(context, 25F), 0, 0)
-
+        val params by lazy { configureLayoutParams(context) }
         this.layoutParams = params
         this.orientation = HORIZONTAL
     }
